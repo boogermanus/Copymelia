fcd2a57c0a76df6537bb84d4be65231313eced0c
Jordan Woodruff <boogermanus@gmail.com>
2025-06-25 17:50:02 -0500

    base class

--- Copymelia.Core/Services/FileProcessor.cs
+++ Copymelia.Core/Services/FileProcessor.cs
@@ -4,7 +4,7 @@ using Directory = System.IO.Directory;
 
 namespace Copymelia.Core.Services;
 
-public class FileProcessor
+public class FileProcessor : FileProcessorBase<FileProcessor>
 {
     private readonly ILogger<FileProcessor> _logger;
     private readonly IEnumerable<string> _imageExtensions = [".jpg", ".jpeg", ".png"];
@@ -30,49 +30,13 @@ public class FileProcessor
     private readonly List<FileInfo> _images = new();
     private readonly List<FileInfo> _documents = new();
 
-    public FileProcessor(ILogger<FileProcessor> logger)
+    public FileProcessor(ILogger<FileProcessor> logger) : base(logger)
     {
         _logger = logger;
     }
 
-    public void Process(string path)
+    protected override void ProcessFile(string filePath)
     {
-        var directories = Directory.EnumerateDirectories(path);
-        var files = Directory.EnumerateFiles(path);
-        ProcessDirectories(directories);
-        ProcessFiles(files);
-    }
-
-    private void ProcessDirectories(IEnumerable<string> directories)
-    {
-        foreach (var directory in directories)
-        {
-            var subDirectories = Directory.GetDirectories(directory);
-            ProcessDirectories(subDirectories);
-            ProcessFiles(Directory.GetFiles(directory));
-        }
-    }
-    
-    private void ProcessFiles(IEnumerable<string> files)
-    {
-        foreach (var file in files)
-        {
-            var info =  new FileInfo(file);
-            if (_imageExtensions.Contains(info.Extension))
-            {
-                _images.Add(info);
-                // var metaDir = ImageMetadataReader.ReadMetadata(file);
-                // foreach (var metaFile in metaDir)
-                //     foreach(var tag in metaFile.Tags)
-                //         _logger.LogDebug($"{tag.Name} - {tag.Name} = {tag.Description}");
-                _logger.LogInformation($"Added image {info.FullName}");
-            }
-
-            if (_documentExtensions.Contains(info.Extension))
-            {
-                _documents.Add(info);
-                _logger.LogInformation($"Added document {info.FullName}");
-            }
-        }
+        throw new NotImplementedException();
     }
 }
