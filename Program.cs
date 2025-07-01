c1f9c7499b40eb2b271892b277c59a9f17306e6b
Jordan Woodruff <boogermanus@gmail.com>
2025-07-01 16:06:46 -0500

    logging needs time to flush

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Copymelia;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = Host.CreateApplicationBuilder();
+host.Logging.AddConsole();
+host.Services.AddSingleton<App>();
+
+var build = host.Build();
+var app = build.Services.GetService<App>();
+app?.Run(args);
+Console.ReadLine();
