ab5d315e1df4cc7a96e606228193fb68bf6c9206
Jordan Woodruff <boogermanus@gmail.com>
2025-07-01 16:15:55 -0500

    Revert "logging needs time to flush"
    This reverts commit c1f9c7499b40eb2b271892b277c59a9f17306e6b.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using Copymelia;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-var host = Host.CreateApplicationBuilder();
-host.Logging.AddConsole();
-host.Services.AddSingleton<App>();
-
-var build = host.Build();
-var app = build.Services.GetService<App>();
-app?.Run(args);
-Console.ReadLine();
+Console.WriteLine("Hello, World!");
