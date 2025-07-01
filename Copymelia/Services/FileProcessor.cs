30e454d421719ebb513b97a073c181eb6200d50e
Jordan Woodruff <boogermanus@gmail.com>
2025-07-01 16:58:02 -0500

    identifying files

--- Copymelia/Services/FileProcessor.cs
+++ Copymelia/Services/FileProcessor.cs
@@ -5,22 +5,16 @@ namespace Copymelia.Services;
 
 public class FileProcessor : FileProcessorBase
 {
-    private readonly IEnumerable<string> _imageExtensions = [".jpg", ".jpeg", ".png"];
+    private readonly IEnumerable<string> _imageExtensions = [".jpg", ".jpeg", ".png", ".tiff"];
     private readonly IEnumerable<string> _documentExtensions =
     [
         ".pdf",
         ".docx",
-        ".docm",
-        ".dotx",
         ".doc",
         ".rtf",
         ".xlsx",
-        ".xlsb",
-        ".xlsm",
-        ".xltx",
         ".xls",
         ".pptx",
-        ".potx",
         ".ppt",
         ".txt"
     ];
@@ -31,10 +25,25 @@ public class FileProcessor : FileProcessorBase
         ".wmv",
         ".mpeg"
     ];
+    private readonly string _musicExtension = ".mp3";
 
     public FileProcessor(ILogger<FileProcessor> logger): base(logger) {}
     protected override void ProcessFile(string file)
     {
+        var info = new FileInfo(file);
+        
+        if(_imageExtensions.Contains(info.Extension))
+            Logger.LogInformation($"Identified image: {info.Name}");
+        
+        if (_documentExtensions.Contains(info.Extension))
+            Logger.LogInformation($"Identified document: {info.Name}");
+        
+        if (_videoExtensions.Contains(info.Extension))
+            Logger.LogInformation($"Identified video: {info.Name}");
+        
+        if(info.Extension == _musicExtension)
+            Logger.LogInformation($"Identified music: {info.Name}");
+        
         Logger.LogInformation($"Processed File: '{file}'");
     }
 }
