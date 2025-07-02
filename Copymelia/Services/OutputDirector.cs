2c645fd4cfd46b2473f8ee6c1e933026d0c7f355
Jordan Woodruff <boogermanus@gmail.com>
2025-07-02 16:12:29 -0500

    output director and broken console

--- Copymelia/Services/OutputDirector.cs
+++ Copymelia/Services/OutputDirector.cs
@@ -6,7 +6,10 @@ namespace Copymelia.Services;
 public class OutputDirector
 {
     private readonly ILogger<OutputDirector> _logger;
-
+    private readonly string _imagesDirectory = "images";
+    private readonly string _documentsDirectory = "documents";
+    private readonly string _videosDirectory = "videos";
+    private readonly string _audioDirectory = "audio";
     public OutputDirector(ILogger<OutputDirector> logger)
     {
         _logger = logger;
@@ -18,6 +21,38 @@ public class OutputDirector
         {
             _logger.LogInformation($"Creating output directory '{options.Output}'");
             Directory.CreateDirectory(options.Output);
+        }
+        
+        var imagesPath = Path.Combine(options.Output, _imagesDirectory);
+
+        if (!Path.Exists(imagesPath))
+        {
+            _logger.LogInformation($"Creating images directory '{imagesPath}'");
+            Directory.CreateDirectory(imagesPath);
+        }
+        
+        var documentsPath = Path.Combine(options.Output, _documentsDirectory);
+
+        if (!Path.Exists(documentsPath))
+        {
+            _logger.LogInformation($"Creating documents directory '{documentsPath}'");
+            Directory.CreateDirectory(documentsPath);
+        }
+        
+        var videosPath = Path.Combine(options.Output, _videosDirectory);
+
+        if (!Path.Exists(videosPath))
+        {
+            _logger.LogInformation($"Creating videos directory '{videosPath}'");
+            Directory.CreateDirectory(videosPath);
+        }
+        
+        var audioPath = Path.Combine(options.Output, _audioDirectory);
+
+        if (!Path.Exists(audioPath))
+        {
+            _logger.LogInformation($"Creating audio directory '{audioPath}'");
+            Directory.CreateDirectory(audioPath);
         }
     }
 }
