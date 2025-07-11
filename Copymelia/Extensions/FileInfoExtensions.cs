05c1ff49b30012a6a9170f13b5555069dafb708e
Jordan Woodruff <boogermanus@gmail.com>
2025-07-11 10:46:22 -0500

    bug fix

--- Copymelia/Extensions/FileInfoExtensions.cs
+++ Copymelia/Extensions/FileInfoExtensions.cs
@@ -11,22 +11,22 @@ public static class FileInfoExtensions
     
     public static bool IsDocument(this FileInfo file)
     {
-        return FileExtensions.DocumentExtensions.Contains(file.Extension);
+        return FileExtensions.DocumentExtensions.Contains(file.Extension.ToLower());
     }
 
     public static bool IsVideo(this FileInfo file)
     {
-        return FileExtensions.VideoExtensions.Contains(file.Extension);
+        return FileExtensions.VideoExtensions.Contains(file.Extension.ToLower());
     }
 
     public static bool IsAudio(this FileInfo file)
     {
-        return FileExtensions.AudioExtension == file.Extension;
+        return FileExtensions.AudioExtension == file.Extension.ToLower();
     }
     
     public static bool IsZip(this FileInfo file)
     {
-        return FileExtensions.ZipExtension == file.Extension;
+        return FileExtensions.ZipExtension == file.Extension.ToLower();
     }
     
     
