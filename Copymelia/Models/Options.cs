4244b86a3ae5f95c1181a9b6118f35c66c3ed35e
Jordan Woodruff <boogermanus@gmail.com>
2025-07-16 15:19:35 -0500

    fixing argument text

--- Copymelia/Models/Options.cs
+++ Copymelia/Models/Options.cs
@@ -4,15 +4,16 @@ namespace Copymelia.Models;
 
 public class Options
 {
-    [Option(Required = true, HelpText = "Path of directory to scan")]
+    [Option(Required = true, HelpText = "Path of directory to scan.")]
     public string Path { get; set; } = string.Empty;
     
-    [Option(Required = true, HelpText = "Path of directory to move files to")]
+    [Option(Required = true, HelpText = @"Path of directory to move files to. 
+Note: if --path contains spaces omit the trailing slash")]
     public string Output { get; set; } = string.Empty;
     
-    [Option(Default = false, Required = false, HelpText = "Scan but do not operate on results")]
+    [Option(Default = false, Required = false, HelpText = "Scan but do not operate on results.")]
     public bool WhatIf { get; set; }
 
-    [Option(Default = "move", Required = false, HelpText = "Mode [move|copy] move by default")]
+    [Option(Default = "move", Required = false, HelpText = "Mode [move|copy].")]
     public string Mode { get; set; } = "move";
 }
